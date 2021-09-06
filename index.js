bdd79bd06752966b213fc9fb6f78f6454fa8e3c1
Mario Daniel Carmen Cruz <89797349+mario69696969@users.noreply.github.com>
2021-09-06 12:31:36 -0500

    Create index.js

---
+++ index.js
@@ -0,0 +1,6 @@
+fuction getIMC(){
+var peso=parseFloat((document.getElementById(´peso´).value);
+                    var altura=parseFloat((document.getElemtById(´altura´).value);
+  var imc=peso/(altura*altura);
+      document-getElementById("IMC").innerHTML=IMC;
+}
